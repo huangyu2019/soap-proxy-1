986454707fcf0b3c578a6c5ff5ee08031741b9d8
huangyu2019 <50135793+huangyu2019@users.noreply.github.com>
2024-08-16 10:33:24 +0800

    设置复杂对象

--- bs-base/bs-soap-provider/src/main/java/com/bluesky/soap/provider/services/ws/entity/User.java
+++ bs-base/bs-soap-provider/src/main/java/com/bluesky/soap/provider/services/ws/entity/User.java
@@ -1,15 +1,24 @@
 package com.bluesky.soap.provider.services.ws.entity;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  * @ClassName User
  * @Description TO DO
  * @Author lenovo
  * @Date 2024/5/5
  * @Version 1.0
- **/
+ **/ 
+@XmlRootElement(name = "user")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class User {
+    @XmlElement(name = "userName", required = true, namespace="http://ws.services.provider.soap.bluesky.com")
     private String userName;
 
+    @XmlElement(name = "age", required = true, namespace="http://ws.services.provider.soap.bluesky.com")
     private int age;
 
     public String getUserName() {
